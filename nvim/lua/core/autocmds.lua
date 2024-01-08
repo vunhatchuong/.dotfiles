933ba4ef33543896fc16f4f2183e9960c0d79948
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-01-08 14:41:39 +0700

    nvim: Update plugins ver, add autocmd

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -1,6 +1,10 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
 -- General Settings
 local general = augroup("General Settings", { clear = true })
 
@@ -90,7 +94,7 @@ autocmd("FileType", {
 })
 
 -- wrap and check for spell in text filetypes
-vim.api.nvim_create_autocmd("FileType", {
+autocmd("FileType", {
     pattern = { "gitcommit", "markdown" },
     callback = function()
         vim.opt_local.wrap = true
@@ -99,7 +103,7 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- Remove trailing whitespaces
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+autocmd({ "BufWritePre" }, {
     group = general,
     pattern = "*",
     callback = function()
@@ -116,3 +120,14 @@ command! -nargs=0 Slinefeed :s/^M$//:
 command! -nargs=0 AddCarriage :%s/\r/\r/g
 ]]
 
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+://") then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
