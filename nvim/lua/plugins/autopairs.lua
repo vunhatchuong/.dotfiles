1db31101b4d644e4a1883b7cdd20d0f53807b6a7
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-01-08 19:37:23 +0700

    Config autoapirs

---
+++ nvim/lua/plugins/autopairs.lua
@@ -0,0 +1,21 @@
+return {
+    "windwp/nvim-autopairs",
+    -- Won't apply ignore_next_char if enabled
+    -- event = "InsertEnter",
+    config = function()
+        require("nvim-autopairs").setup({
+            map_char = {
+                all = "(",
+                tex = "{",
+            },
+            check_ts = true,
+            ts_config = {
+                lua = { "string", "source" },
+                javascript = { "string", "template_string" },
+                java = false,
+            },
+            disable_filetype = { "TelescopePrompt"},
+            ignored_next_char = string.gsub([[ [%w%%%'%[%"%.] ]], "%s+", ""),
+        })
+    end,
+}
