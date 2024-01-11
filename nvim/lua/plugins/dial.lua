5537c73c7bdf35a7370b5be341c2447d6c60c465
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-01-11 14:47:02 +0700

    Nvim: Add Toggleterm

--- nvim/lua/plugins/dial.lua
+++ nvim/lua/plugins/dial.lua
@@ -1,27 +1,41 @@
-  -- better increase/descrease
-return  {
+-- better increase/descrease
+return {
     "monaqa/dial.nvim",
     keys = {
-        { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-        { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+        {
+            "<C-a>",
+            function()
+                return require("dial.map").inc_normal()
+            end,
+            expr = true,
+            desc = "Increment",
+        },
+        {
+            "<C-x>",
+            function()
+                return require("dial.map").dec_normal()
+            end,
+            expr = true,
+            desc = "Decrement",
+        },
     },
     config = function()
         local augend = require("dial.augend")
-            require("dial.config").augends:register_group({
-                default = {
-                    augend.integer.alias.decimal,
-                    augend.integer.alias.hex,
-                    augend.date.alias["%Y/%m/%d"],
-                    augend.date.alias["%d/%m/%Y"],
-                    augend.constant.alias.bool,
-                    augend.semver.alias.semver,
-                    augend.constant.new({ elements = { "let", "const" } }),
-                    augend.constant.new{
-                        elements = {"&&", "||"},
-                        word = false,
-                        cyclic = true,
-                },
-              },
-            })
+        require("dial.config").augends:register_group({
+            default = {
+                augend.integer.alias.decimal,
+                augend.integer.alias.hex,
+                augend.date.alias["%Y/%m/%d"],
+                augend.date.alias["%d/%m/%Y"],
+                augend.constant.alias.bool,
+                augend.semver.alias.semver,
+                augend.constant.new({ elements = { "let", "const" } }),
+                augend.constant.new({
+                    elements = { "&&", "||" },
+                    word = false,
+                    cyclic = true,
+                }),
+            },
+        })
     end,
-  }
+}
