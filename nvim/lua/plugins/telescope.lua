5aa24888c52558dd4f3c57f0297ecf30c98b3e5a
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-02-04 16:05:49 +0700

    nvim: Update telescope logic

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -28,33 +28,36 @@ return {
                 end,
             },
         },
-        -- stylua: ignore
         keys = {
             {
                 "<leader><space>",
                 function()
                     local activeClients = vim.lsp.get_active_clients()
-
-                    local workspaceFolder
-                    if activeClients[2] then
-                        workspaceFolder = activeClients[2].config.root_dir
-                        require("telescope.builtin").find_files({
-                            cwd = workspaceFolder,
-                        })
-                        return
-                    else
-                        require("telescope.builtin").find_files()
+                    for i, client in ipairs(activeClients) do
+                        if activeClients[i].name ~= "null-ls" then
+                            require("telescope.builtin").find_files({
+                                cwd = client.config.root_dir,
+                                prompt_title = client.name .. " Find Files",
+                            })
+                            return
+                        end
                     end
+                    require("telescope.builtin").find_files({
+                        cwd = vim.fn.expand("%:p:h"),
+                        prompt_title = "Default Find Files",
+                    })
                 end,
                 desc = "Find Files",
             },
 
+            -- stylua: ignore start
             { "<leader>f?", function() require("telescope.builtin").oldfiles() end,     desc = "Find recently opened Files" },
             { "<leader>ft", function() require("telescope.builtin").live_grep() end,    desc = "[F]ind [T]ext" },
             { "<leader>fs", function() require("telescope.builtin").grep_string() end,  desc = "[F]ind [S]tring" },
             { "<leader>fk", function() require("telescope.builtin").keymaps() end,      desc = "Keymaps" },
             { "<leader>fg", function() require("telescope.builtin").git_files() end,    desc = "Find Git Files" },
             { "<leader>gc", function() require("telescope.builtin").git_branches() end, desc = "Checkout branches" },
+            -- stylua: ignore end
         },
         opts = {
             defaults = {
