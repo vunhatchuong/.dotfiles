ab2f4ec99846fc481badc9205fbe1282ea8f7e8a
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-03-02 14:21:27 +0700

    nvim(mason): Add codespell

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -9,6 +9,7 @@ return {
             ensure_installed = {
                 "lua-language-server",
                 "stylua",
+                "codespell",
 
                 "html-lsp",
                 "prettier",
