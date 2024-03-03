7bb4ed034d80631902cfbf7d916b1271f0ddcf5d
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-03-03 08:52:58 +0700

    zsh(feat): Add micromamba

--- zsh/conda.zsh
+++ zsh/conda.zsh
@@ -4,12 +4,29 @@
 #if [ $? -eq 0 ]; then
 #    eval "$__conda_setup"
 #else
-if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
-    . "$HOME/miniconda3/etc/profile.d/conda.sh"
-else
-    export PATH="$HOME/miniconda3/bin:$PATH"
+if command -v "conda" > /dev/null; then
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/miniconda3/bin:$PATH"
+    fi
 fi
-conda activate base
 #fi
 #unset __conda_setup
 # <<< conda initialize <<<
+
+if command -v "micromamba" > /dev/null; then
+    # >>> mamba initialize >>>
+    # !! Contents within this block are managed by 'mamba init' !!
+    export MAMBA_EXE="$HOME/.local/bin/micromamba";
+    export MAMBA_ROOT_PREFIX="$HOME/micromamba";
+    __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__mamba_setup"
+    else
+        alias micromamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+    fi
+    unset __mamba_setup
+    # <<< mamba initialize <<<
+    alias mamba="micromamba"
+fi
