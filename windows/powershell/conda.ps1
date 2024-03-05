61841af03ab9fb0d72cdd077841f6a192b946986
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-03-05 21:23:49 +0700

    windows(conda): Add conda as alias to micromamba

--- windows/powershell/conda.ps1
+++ windows/powershell/conda.ps1
@@ -5,6 +5,7 @@ if (Test-Path "$HOME\AppData\Local\micromamba\micromamba.exe")
     (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -p $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
 
     bashAlias mamba "micromamba @args"
+    bashAlias conda "micromamba @args"
 } elseif (Test-Path "$HOME\miniconda3\Scripts\conda.exe")
 {
     (& "$HOME\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
