41b4f1da3d041dc833f7d63e395136685b1c2cc1
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-03-05 09:42:35 +0700

    windows(conda): Add comments

--- windows/Microsoft.PowerShell_profile.ps1
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -2,19 +2,14 @@
 
 if (Test-Path "C:\Users\ronny\AppData\Local\micromamba\micromamba.exe")
 {
-    #region mamba initialize
-    # !! Contents within this block are managed by 'mamba shell init' !!
     $Env:MAMBA_ROOT_PREFIX = "C:\Users\ronny\micromamba"
     $Env:MAMBA_EXE = "C:\Users\ronny\AppData\Local\micromamba\micromamba.exe"
     (& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -p $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
+
     bashAlias mamba "micromamba @args"
-    #endregion
 } elseif (Test-Path "C:\Users\ronny\miniconda3\Scripts\conda.exe")
 {
-    #region conda initialize
-    # !! Contents within this block are managed by 'conda init' !!
     (& "C:\Users\ronny\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-    #endregion
 }
 
 Invoke-Expression (&starship init powershell)
