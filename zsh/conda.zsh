10b24b89150ec67613f4b0eba7df8d4dbba7876f
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-03-05 09:42:07 +0700

    zsh(conda): Prioritize micromamba

--- zsh/conda.zsh
+++ zsh/conda.zsh
@@ -1,10 +1,7 @@
-if command -v "conda" > /dev/null; then
-    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "$HOME/miniconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="$HOME/miniconda3/bin:$PATH"
-    fi
-elif command -v "micromamba" > /dev/null; then
+if command -v "micromamba" > /dev/null; then
+
+    # >>> mamba initialize >>>
+    # !! Contents within this block are managed by 'mamba init' !!
     export MAMBA_EXE="$HOME/.local/bin/micromamba";
     export MAMBA_ROOT_PREFIX="$HOME/micromamba";
     __mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
@@ -14,5 +11,14 @@ elif command -v "micromamba" > /dev/null; then
         alias micromamba="$MAMBA_EXE"
     fi
     unset __mamba_setup
+    # >>> mamba initialize >>>
+
     alias mamba="micromamba"
+    alias conda="mamba"
+elif  command -v "conda" > /dev/null; then
+    if [ -f "$HOME/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "$HOME/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="$HOME/miniconda3/bin:$PATH"
+    fi
 fi
