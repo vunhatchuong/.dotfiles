f2443fa1244fa62c0e4955d657c2f052cac1a89a
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-06-01 23:00:52 +0700

    nvim(comment): Fix bind not work on Windows

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -26,7 +26,12 @@ keymap("n", "c", '"_c', opts)
 keymap("n", "C", '"_C', opts)
 keymap("n", "x", '"_x', opts)
 keymap("n", "zo", "za", opts)
-keymap({ "n", "x", "o" }, "<C-/>", "gcc", { remap = true })
+
+if vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
+    keymap({ "n", "x", "o" }, "<C-_>", "gcc", { remap = true })
+else
+    keymap({ "n", "x", "o" }, "<C-/>", "gcc", { remap = true })
+end
 
 -- Remap for dealing with word wrap
 keymap("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
