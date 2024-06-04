1432d55f6d71d628d0472cfd343a9ca09b429392
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-06-04 10:52:05 +0700

    nvim(lsp): Add codelens keymap

--- nvim/lua/plugins/lsp/keymaps.lua
+++ nvim/lua/plugins/lsp/keymaps.lua
@@ -15,14 +15,20 @@ function M.on_attach()
 
     nmap("K", vim.lsp.buf.hover, "Hover Documentation")
     nmap("<C-k>", vim.diagnostic.open_float, "Open floating diagnostic message")
+
     nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]definition")
     nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
     nmap("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
     nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]definition")
     nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
     nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]symbols")
     nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]symbols")
+
     -- nmap("<leader>sd", require("telescope.builtin").diagnostics, "[S]earch [D]iagnostics")
+
+    nmap("<leader>cc", vim.lsp.codelens.run, "Run Codelens")
+    nmap("<leader>cC", vim.lsp.codelens.refresh, "Refresh & Display Codelens")
     -- stylua: ignore end
 end
 
