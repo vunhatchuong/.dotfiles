ca644fdb97999b95e28423d2d5c980d2c49b0317
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-07-09 11:22:33 +0700

    nvim(notify): Changes from mini to fidget

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,28 +1,32 @@
 return {
-    "echasnovski/mini.notify",
-    event = { "VeryLazy" },
-    config = function()
-        local format_config = function(notif)
-            return notif.msg
-        end
-        local win_config = function()
-            local has_statusline = vim.o.laststatus > 0
-            local bottom_space = vim.o.cmdheight + (has_statusline and 1 or 0)
-            return {
-                anchor = "SE",
-                col = vim.o.columns,
-                row = vim.o.lines - bottom_space,
-                border = "none",
-            }
-        end
-        require("mini.notify").setup({
-            content = {
-                format = format_config,
-            },
-            window = {
-                config = win_config,
-                winblend = 100,
-            },
-        })
-    end,
+    -- {
+    --     "echasnovski/mini.notify",
+    --     event = { "VeryLazy" },
+    --     config = function()
+    --         local format_config = function(notif)
+    --             return notif.msg
+    --         end
+    --         local win_config = function()
+    --             local has_statusline = vim.o.laststatus > 0
+    --             local bottom_space = vim.o.cmdheight
+    --                 + (has_statusline and 1 or 0)
+    --             return {
+    --                 anchor = "SE",
+    --                 col = vim.o.columns,
+    --                 row = vim.o.lines - bottom_space,
+    --                 border = "none",
+    --             }
+    --         end
+    --         require("mini.notify").setup({
+    --             content = {
+    --                 format = format_config,
+    --             },
+    --             window = {
+    --                 config = win_config,
+    --                 winblend = 100,
+    --             },
+    --         })
+    --     end,
+    -- },
+    { "j-hui/fidget.nvim", event = { "VeryLazy" }, opts = {} },
 }
