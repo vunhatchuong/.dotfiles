95fc6b2c508ee7b3e4f4e5ff6a8544b74cd68d7c
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-07-17 00:46:38 +0700

    nvim(lsp): Add code action actions-preview.nvim

--- nvim/lua/plugins/lsp/addons.lua
+++ nvim/lua/plugins/lsp/addons.lua
@@ -130,4 +130,31 @@ return {
             highlight_node_at_cursor = true,
         },
     },
+    {
+        "aznhe21/actions-preview.nvim",
+        keys = {
+            {
+                "<leader>cA",
+                mode = { "n", "v" },
+                function()
+                    require("actions-preview").code_actions()
+                end,
+            },
+        },
+        opts = {
+            telescope = {
+                sorting_strategy = "ascending",
+                layout_strategy = "vertical",
+                layout_config = {
+                    width = 0.8,
+                    height = 0.9,
+                    prompt_position = "top",
+                    preview_cutoff = 20,
+                    preview_height = function(_, _, max_lines)
+                        return max_lines - 15
+                    end,
+                },
+            },
+        },
+    },
 }
