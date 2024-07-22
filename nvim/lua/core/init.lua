060a8a384bb731f9c1f933a14fef1f817d4c8d9b
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-07-22 19:02:44 +0700

    nvim(refactor): Ordering of require in init.lua

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -1,4 +1,4 @@
-require "core.options"
-require "core.autocmds"
-require "core.keymaps"
-require "core.lazy"
+require("core.autocmds")
+require("core.keymaps")
+require("core.options")
+require("core.lazy")
