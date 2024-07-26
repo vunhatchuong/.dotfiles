16675f27ccf898ad169eb0a5ae532ca2cdf0c153
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-07-26 20:01:08 +0700

    nvim(indent-line): Removed

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -1,36 +1,7 @@
 return {
     {
-        "lukas-reineke/indent-blankline.nvim",
-        event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-        opts = {
-            indent = {
-                char = "│",
-                tab_char = "│",
-            },
-            scope = { enabled = false },
-            exclude = {
-                filetypes = {
-                    "netrw",
-                    "help",
-                    "lspinfo",
-                    "lazy",
-                    "mason",
-                    "oil",
-                    "trouble",
-                    "harpoon",
-                    "TelescopePrompt",
-                    "notify",
-                    "undotree",
-                    "grapple",
-                    "toggleterm",
-                },
-                buftypes = { "terminal", "nofile" },
-            },
-        },
-        main = "ibl",
-    },
-    {
         "echasnovski/mini.indentscope",
+        cmd = "AerialToggle",
         event = { "BufReadPost", "BufNewFile", "BufWritePre" },
         opts = {
             symbol = "│",
