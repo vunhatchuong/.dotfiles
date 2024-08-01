2039ffb511968dd899e1a35e48871ee11f5fd8d0
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-08-01 12:33:14 +0700

    zsh: Add Ctrl+z toggle

--- linux/zsh/keymap.zsh
+++ linux/zsh/keymap.zsh
@@ -1,1 +1,14 @@
 bindkey -s "^f" "tmux-sessionizer\n"
+
+# Ctrl-z to both suspend and start, don't have to type fg
+fancy-ctrl-z () {
+  if [[ $#BUFFER -eq 0 ]]; then
+    BUFFER="fg"
+    zle accept-line -w
+  else
+    zle push-input -w
+    zle clear-screen -w
+  fi
+}
+zle -N fancy-ctrl-z
+bindkey "^Z" fancy-ctrl-z
