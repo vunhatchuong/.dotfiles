9af080397c3a6836d348cfd1464f01b7a89d3988
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-08-01 18:11:44 +0700

    nvim(theme): Adjust themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -5,94 +5,30 @@ return {
         opts = {
             layout = "float",
             schemes = {
-                "catppuccin-mocha",
+                "default",
                 "rose-pine",
                 "rosebones",
-                "no-clown-fiesta",
-                "monochrome",
-                "sequoia",
                 "eldritch",
                 "lackluster-hack",
                 "Duskfox",
                 "Carbonfox",
-                "moonfly",
-                "oxocarbon",
                 "blue-moon",
-                "crimson_moonlight",
                 "nordic",
+                "sequoia-moonlight",
                 "palette",
+                "sequoia",
+                "flow",
+                "dogrun",
+                "crimson_moonlight",
+                "sakura",
+                "darkvoid",
             },
             window = {
                 border = "rounded",
             },
         },
     },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        config = function()
-            require("catppuccin").setup({
-                flavor = "mocha",
-                -- transparent_background = true, -- disables setting the background color.
-                integrations = {
-                    treesitter = true,
-                    treesitter_context = true,
-                    semantic_tokens = true,
-                    cmp = true,
-                    gitsigns = true,
-                    harpoon = true,
-                    illuminate = true,
-                    leap = true,
-                    lsp_trouble = true,
-                    mason = true,
-                    markdown = true,
-                    indent_blankline = {
-                        enabled = true,
-                        scope_color = "",
-                        colored_indent_levels = true,
-                    },
-                    mini = {
-                        enabled = true,
-                        indentscope_color = "pink",
-                    },
-                    native_lsp = {
-                        enabled = true,
-                        virtual_text = {
-                            errors = { "italic" },
-                            hints = { "italic" },
-                            warnings = { "italic" },
-                            information = { "italic" },
-                        },
-                        underlines = {
-                            errors = { "underline" },
-                            hints = { "underline" },
-                            warnings = { "underline" },
-                            information = { "underline" },
-                        },
-                        inlay_hints = {
-                            background = true,
-                        },
-                    },
-                    rainbow_delimiters = true,
-                    telescope = {
-                        enabled = true,
-                    },
-                    fidget = true,
-                    dap = true,
-                    dap_ui = true,
-                    neotest = true,
-                },
-                -- Use command :hi to check out highlight groups
-                custom_highlights = function(colors)
-                    return {
-                        IncSearch = { bg = colors.flamingo },
-                    }
-                end,
-            })
-        end,
-    },
     { "rose-pine/neovim", name = "rose-pine" },
-    { "aktersnurra/no-clown-fiesta.nvim" },
     {
         "mcchrish/zenbones.nvim",
         dependencies = {
@@ -100,16 +36,14 @@ return {
             cmd = { "Lushify" },
         },
     },
-    { "kdheepak/monochrome.nvim" },
     { "Hiroya-W/sequoia-moonlight.nvim" },
     { "eldritch-theme/eldritch.nvim" },
     { "slugbyte/lackluster.nvim" },
     { "EdenEast/nightfox.nvim" },
-    { "bluz71/vim-moonfly-colors", name = "moonfly" },
-    { "nyoom-engineering/oxocarbon.nvim" },
     { "kyazdani42/blue-moon" },
     { "rockerBOO/boo-colorscheme-nvim" },
     { "AlexvZyl/nordic.nvim" },
+    { dir = "D:/sequoia-monochrome", lazy = true },
     {
         "roobert/palette.nvim",
         config = function()
@@ -163,4 +97,16 @@ return {
             })
         end,
     },
+    {
+        "0xstepit/flow.nvim",
+        opts = {
+            transparent = true,
+            fluo_color = "pink",
+            mode = "normal",
+            aggressive_spell = false,
+        },
+    },
+    { "wadackel/vim-dogrun" },
+    { "anAcc22/sakura.nvim" },
+    { "aliqyan-21/darkvoid.nvim" },
 }
