252bb91835d1cbd33318ab3c72bc0eaa0e5a6a62
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-08-03 10:09:26 +0700

    hyprland: Update exec-once and add udiskie

--- linux/hypr/scripts/resetxdgportal.sh
+++ linux/hypr/scripts/resetxdgportal.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # https://wiki.hyprland.org/FAQ/#some-of-my-apps-take-a-really-long-time-to-open
-sleep 4
-killall -e xdg-desktop-portal-wlr
+sleep 1
+killall xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-kde
+killall xdg-desktop-portal-lxqt
+killall xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-wlr &
-sleep 4
+sleep 1
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep 2
 /usr/lib/xdg-desktop-portal &
