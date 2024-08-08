d0c86da3824da5f1d4bd891914069e489e128638
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-08-08 16:37:58 +0700

    Feat: Uses Version Fox instead of fnm, gobrew

--- linux/zsh/export.zsh
+++ linux/zsh/export.zsh
@@ -12,19 +12,6 @@ setopt hist_verify            # show command with history expansion to user befo
 setopt share_history          # share command history data
 setopt HIST_REDUCE_BLANKS    # Remove unnecessary blank lines.
 
-export JAVA_HOME='/usr/lib/jvm/java-21-openjdk'
-export PATH=$JAVA_HOME/bin:$PATH
-
-export M2_HOME=/usr/local/apache-maven/apache-maven-3.9.1
-export PATH="$M2_HOME/bin:$PATH"
-
-export PATH="$HOME/.cargo/bin:$PATH"
-
-export PATH=$PATH:/usr/local/go/bin
-export PATH="$HOME/go/bin:$PATH"
-export PATH="$HOME/.gobrew/current/bin:$HOME/.gobrew/bin:$PATH"
-export GOROOT="$HOME/.gobrew/current/go"
-
 VIM="nvim"
 
 # FZF Catppuccin theme
@@ -33,10 +20,6 @@ export FZF_DEFAULT_OPTS=" \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-# Start starship prompt and zoxide
 eval "$(starship init zsh)"
 eval "$(zoxide init zsh)"
-
-# fnm
-#export PATH="$HOME/.local/share/fnm:$PATH" # Need or not?
-eval "$(fnm env --use-on-cd)"
+eval "$(vfox activate zsh)"
