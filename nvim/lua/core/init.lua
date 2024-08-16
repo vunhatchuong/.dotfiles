73ddca92995da5237f3f612365b6a297cbb7c78b
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-08-17 06:13:12 +0700

    nvim(theme): Update color of default nvim theme

--- nvim/lua/core/init.lua
+++ nvim/lua/core/init.lua
@@ -2,3 +2,12 @@ require("core.autocmds")
 require("core.keymaps")
 require("core.options")
 require("core.lazy")
+
+-- Modified default theme
+-- https://github.com/glepnir/nvim/blob/main/lua/core/init.lua
+vim.cmd([[
+hi link @property @variable
+hi Type guifg=#eed49f ctermfg=11
+hi link @type.builtin Type
+hi link @type Type
+]])
