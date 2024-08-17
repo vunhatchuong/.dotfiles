44c2dbeb973819c93e432fe60a7c31456106fb18
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-08-17 15:26:15 +0700

    zsh(completion): Show hidden files in menu

--- linux/zsh/completion.zsh
+++ linux/zsh/completion.zsh
@@ -13,6 +13,7 @@ done
 compinit -C
 
 # setopt GLOB_COMPLETE      # Show autocompletion menu with globs
+setopt GLOB_DOTS
 setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
 setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
 setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
