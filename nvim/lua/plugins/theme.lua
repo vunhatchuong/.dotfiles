28300a0dcac83d1effff410ab090944d153ae2a1
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-08-19 06:59:35 +0700

    nvim(theme): Update theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -6,17 +6,15 @@ return {
             layout = "float",
             schemes = {
                 "default",
-                "rosebones",
-                "eldritch",
                 "lackluster-hack",
                 "Duskfox",
                 "blue-moon",
                 "nordic",
-                "sequoia-moonlight",
-                "palette",
                 "sequoia",
+                "eldritch",
                 "flow",
                 "dogrun",
+                "rosebones",
                 "crimson_moonlight",
                 "sakura",
                 "darkvoid",
@@ -34,64 +32,11 @@ return {
         },
     },
     { "Hiroya-W/sequoia-moonlight.nvim" },
-    { "eldritch-theme/eldritch.nvim" },
     { "slugbyte/lackluster.nvim" },
     { "kyazdani42/blue-moon" },
     { "rockerBOO/boo-colorscheme-nvim" },
     { "AlexvZyl/nordic.nvim" },
-    {
-        "roobert/palette.nvim",
-        config = function()
-            require("palette").setup({
-                caching = true,
-                palettes = {
-                    main = "sequoia_monochrome",
-                    accent = "sequoia_monochrome",
-                    state = "sequoia_monochrome",
-                },
-
-                italics = true,
-                transparent_background = false,
-
-                custom_palettes = {
-                    main = {
-                        sequoia_monochrome = {
-                            color0 = "#0F1014", -- BG
-                            color1 = "#1F1F24", -- Visual, CursorLine BG
-                            color2 = "#868690", -- Cursor, Fold
-                            color3 = "#626983", -- Keyword, LineNumber
-                            color4 = "#626983", -- Delimiter
-                            color5 = "#868690", -- Identifier
-                            color6 = "#43444D", -- Comments
-                            color7 = "#B6BAC8", -- Function
-                            color8 = "#D3D5DE", -- String
-                        },
-                    },
-                    accent = {
-                        sequoia_monochrome = {
-                            accent0 = "#B6BAC8", -- Number
-                            accent1 = "#575861", -- Search, Visual text
-                            accent2 = "#FFEE55", -- ??
-                            accent3 = "#43444D", -- MatchParen
-                            accent4 = "#B6BAC8", -- Bool, Float, Const
-                            accent5 = "#50AFFF", -- ??
-                            accent6 = "#FF5A8F", -- ??
-                            accent7 = "#D45AFA", -- ??
-                        },
-                    },
-                    state = {
-                        sequoia_monochrome = {
-                            error = "#A6002F",
-                            warning = "#ea9d34",
-                            hint = "#907aa9",
-                            ok = "#006c42",
-                            info = "#31748f",
-                        },
-                    },
-                },
-            })
-        end,
-    },
+    { "eldritch-theme/eldritch.nvim" },
     {
         "0xstepit/flow.nvim",
         opts = {
