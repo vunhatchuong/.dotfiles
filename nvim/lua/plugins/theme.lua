4d04ffa48f435bdf3c3491f6895c8cf1dd726827
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-12-27 12:53:29 +0700

    nvim(theme): Change from scheming to themify.nvim

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,72 +1,68 @@
 return {
     {
-        "wllfaria/scheming.nvim",
+        "lmantw/themify.nvim",
         lazy = false,
-        init = function()
-            -- Manually triggers ColorScheme event to run related autocmds
-            -- Since scheming.nvim doesn't run vim.cmd("colorscheme theme")
-            vim.cmd("doautocmd ColorScheme")
-        end,
-        opts = {
-            layout = "float",
-            schemes = {
-                "default",
-                "gruber-darker",
-                "lackluster-hack",
-                "lackluster-mint",
-                "blue-moon",
-                "nordic",
-                "sequoia",
-                "eldritch",
-                "flow",
-                "dogrun",
-                "rosebones",
-                "crimson_moonlight",
-                "sakura",
-                "darkvoid",
-                "simple-dark",
+        config = {
+            "default",
+            {
+                "mcchrish/zenbones.nvim",
+                before = function()
+                    vim.g.bones_compat = 1
+                end,
+                -- whitelist = { "tokyonight-night", "tokyonight-day" },
+                blacklist = { "randombones" },
             },
-            window = { border = vim.g.border_style },
+            "Hiroya-W/sequoia-moonlight.nvim",
+            { "slugbyte/lackluster.nvim" },
+            { "kyazdani42/blue-moon" },
+            { "rockerBOO/boo-colorscheme-nvim" },
+            { "AlexvZyl/nordic.nvim" },
+            { "eldritch-theme/eldritch.nvim" },
+            { "wadackel/vim-dogrun" },
+            { "anAcc22/sakura.nvim" },
+            { "aliqyan-21/darkvoid.nvim" },
+            { "blazkowolf/gruber-darker.nvim" },
+            { "tek256/simple-dark" },
+            { "kvrohit/substrata.nvim" },
+            { "mcauley-penney/ice-cave.nvim" },
+            { "aktersnurra/no-clown-fiesta.nvim" },
+            {
+                "0xstepit/flow.nvim",
+                before = function()
+                    local ok, flow = pcall(require, "flow")
+                    if ok then
+                        flow.setup({
+                            theme = {
+                                style = "dark", --  "dark" | "light"
+                                contrast = "default", -- "default" | "high"
+                                transparent = false, -- true | false
+                            },
+                            colors = {
+                                mode = "default", -- "default" | "dark" | "light"
+                                fluo = "pink", -- "pink" | "cyan" | "yellow" | "orange" | "green"
+                            },
+                            ui = {
+                                borders = "inverse", -- "theme" | "inverse" | "fluo" | "none"
+                                aggressive_spell = false, -- true | false
+                            },
+                        })
+                    end
+                end,
+            },
+
+            {
+                "cvigilv/patana.nvim",
+                branch = "develop",
+                before = function()
+                    vim.g.patana_high_contrast = true
+                    vim.g.patana_colored_statusline = true
+                    vim.g.patana_oob_filetypes =
+                        { "qf", "lazy", "mason", "help" }
+                end,
+            },
+            { "ramojus/mellifluous.nvim", branch = "v1" },
+            { "dgox16/oldworld.nvim" },
+            { "wnkz/monoglow.nvim" },
         },
     },
-    {
-        "mcchrish/zenbones.nvim",
-        init = function()
-            vim.g.bones_compat = 1
-        end,
-    },
-    { "Hiroya-W/sequoia-moonlight.nvim" },
-    { "slugbyte/lackluster.nvim" },
-    { "kyazdani42/blue-moon" },
-    { "rockerBOO/boo-colorscheme-nvim" },
-    { "AlexvZyl/nordic.nvim" },
-    { "eldritch-theme/eldritch.nvim" },
-    {
-        "0xstepit/flow.nvim",
-        opts = {
-            transparent = true,
-            fluo_color = "pink",
-            mode = "normal",
-            aggressive_spell = false,
-        },
-    },
-    { "wadackel/vim-dogrun" },
-    { "anAcc22/sakura.nvim" },
-    { "aliqyan-21/darkvoid.nvim" },
-    { "blazkowolf/gruber-darker.nvim" },
-    { "tek256/simple-dark" },
-    { "kvrohit/substrata.nvim" },
-    { "mcauley-penney/ice-cave.nvim" },
-    { "aktersnurra/no-clown-fiesta.nvim" },
-    { "ramojus/mellifluous.nvim", branch = "v1" },
-    {
-        "cvigilv/patana.nvim",
-        branch = "develop",
-        init = function()
-            vim.g.patana_high_contrast = true
-            vim.g.patana_colored_statusline = true
-            vim.g.patana_oob_filetypes = { "qf", "lazy", "mason", "help" }
-        end,
-    },
-    { "dgox16/oldworld.nvim" },
 }
