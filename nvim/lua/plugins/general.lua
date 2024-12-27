1c2252236edbbd854f9afc4eb25dc827df98b41b
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-12-27 07:07:30 +0700

    nvim(general): update xylene.nvim opts

--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -252,7 +252,29 @@ return {
     {
         "Tronikelis/xylene.nvim",
         cmd = { "Xylene" },
-        opts = {},
+        opts = {
+            get_cwd = function()
+                if vim.bo.filetype == "oil" then
+                    return require("oil").get_current_dir()
+                end
+                return vim.fn.getcwd()
+            end,
+            get_current_file_dir = function()
+                if vim.bo.filetype == "oil" then
+                    return require("oil").get_current_dir()
+                end
+                return vim.fn.expand("%:p")
+            end,
+            on_attach = function(renderer)
+                vim.keymap.set("n", "<cr>", function()
+                    renderer:toggle(vim.api.nvim_win_get_cursor(0)[1])
+                end, { buffer = renderer.buf })
+
+                vim.keymap.set("n", "!", function()
+                    renderer:toggle_all(vim.api.nvim_win_get_cursor(0)[1])
+                end, { buffer = renderer.buf })
+            end,
+        },
     },
     { -- Breaks numb.nvim and blink.nvim
         "smilhey/ed-cmd.nvim",
