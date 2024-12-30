3620848153493c59f23276ef08f3eae874dfeb61
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-12-30 21:02:56 +0700

    nvim(markdown): constrain keymaps to .md

--- nvim/lua/plugins/lang/general/markdown.lua
+++ nvim/lua/plugins/lang/general/markdown.lua
@@ -123,22 +123,21 @@ return {
     {
         "gaoDean/autolist.nvim",
         ft = { "markdown", "text", "tex", "plaintex", "norg" },
-        config = function()
-            require("autolist").setup()
-
-            vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
-            vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
-            vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
-            vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
-            vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
-            vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
-            vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
-
+        -- stylua: ignore
+        keys = {
+            { mode = { "i" }, "<TAB>", "<CMD>AutolistTab<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { mode = { "i" }, "<S-TAB>", "<CMD>AutolistShiftTab<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { mode = { "i" }, "<CR>", "<CR><CMD>AutolistNewBullet<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { "o", "o<CMD>AutolistNewBullet<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { "O", "O<CMD>AutolistNewBulletBefore<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { "<CR>", "<CMD>AutolistToggleCheckbox<CR><CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
             -- functions to recalculate list on edit
-            vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
-            vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
-        end,
+            { "<C-R>", "<CMD>AutolistRecalculate<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { ">>", ">><CMD>AutolistRecalculate<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { "<<", "<<<CMD>AutolistRecalculate<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { "dd", "dd<CMD>AutolistRecalculate<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+            { mode = { "v" }, "d", "d<CMD>AutolistRecalculate<CR>", ft = { "markdown", "text", "tex", "plaintex", "norg" } },
+        },
+        opts = {},
     },
 }
