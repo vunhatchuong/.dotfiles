4b252dd514f57da1f6f9fd496460cee54dfdc0b1
Vu Nhat Chuong <ronnyvu321@gmail.com>
2024-12-31 07:10:59 +0700

    zsh(function): add actually.nvim

--- linux/zsh/function.zsh
+++ linux/zsh/function.zsh
@@ -26,3 +26,62 @@ function port() {
         lsof -nP -iTCP"$port_filter" -sTCP:$state
     fi
 }
+
+
+# https://github.com/mong8se/actually.fish
+_actually() {
+    local last_status=$?
+    if (( last_status == 0 )); then
+        return 0
+    fi
+
+  # Get the last executed command
+    local last_cmd
+    last_cmd=(${(z)$(fc -ln -1)})
+    if [[ ${last_cmd[1]} != "cd" ]]; then
+        return $last_status
+    fi
+
+
+    # Perform tilde expansion manually
+    local last_dir last_base
+    last_dir=$(dirname "${last_cmd[2]//~/$HOME}")
+    last_base=$(basename "${last_cmd[2]}")
+
+    local list=($(find "$last_dir" -maxdepth 1 -mindepth 1 -type d -iname "$last_base"'*' | sort))
+
+    if [[ ${#list[@]} -eq 0 ]]; then
+        return $last_status
+    fi
+
+    # Selection
+    local destination
+    if command -v fzf &>/dev/null; then
+        local preview
+    if command -v eza &>/dev/null; then
+        preview='eza -1 --color=always {}'
+    else
+        preview='ls -F --color=auto {}'
+    fi
+        destination=$(printf '%s\0' "${list[@]}" | fzf --read0 --layout reverse --tiebreak=begin --preview="$preview")
+    else
+        for i in "${!list[@]}"; do
+            echo "$((i + 1)) ${list[i]}"
+        done
+        echo -ne "\e[32m> \e[0m"
+        read -r answer
+            if [[ $answer =~ ^[0-9]+$ ]] && (( answer > 0 && answer <= ${#list[@]} )); then
+                destination="${list[answer - 1]}"
+            fi
+    fi
+
+    if [[ -d $destination ]]; then
+        cd "$destination" || return $last_status
+    else
+        return $last_status
+    fi
+}
+
+# Attach Zsh hook
+autoload -Uz add-zsh-hook
+add-zsh-hook precmd _actually
--- linux/zsh/general.zsh
+++ linux/zsh/general.zsh
@@ -1,3 +1,4 @@
+# man zshparam
 export KEYTIMEOUT=1           # Reduce input lag
 bindkey -e                    # Disable vim-mode, breaks term in nvim
 
@@ -10,3 +11,7 @@ setopt NOMATCH                # Throw error if no files match pattern
 setopt INTERACTIVE_COMMENTS   # Allow comments even in interactive shells.
 unsetopt CASE_GLOB            # Disable case sensitivity in globbing
 unsetopt CASE_MATCH           # Disable case sensitivity in pattern matching
+
+setopt correct                # Use CORRECT_IGNORE to ignore words
+# setopt correctall
+SPROMPT="Correct %B%F{red}%U%R%b%f%u to %F{green}%r%f? [%By|%Bn|%Be|%Ba]: "
