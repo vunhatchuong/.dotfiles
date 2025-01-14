9ad6c856e9262a132db39ab18136b838612bb97c
Vu Nhat Chuong <ronnyvu321@gmail.com>
2025-01-14 17:58:22 +0700

    feat(theme): themify-extra daylight

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -88,6 +88,9 @@ return {
                 enable = true,
                 daily = true,
             },
+            daylight = {
+                enable = true,
+            },
         },
     },
 }
