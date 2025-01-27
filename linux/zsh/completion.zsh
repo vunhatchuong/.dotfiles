41058a9371d392e5e22d82d638dc4907809b848f
Vu Nhat Chuong <ronnyvu321@gmail.com>
2025-01-27 13:57:49 +0700

    zsh: refactor

--- linux/zsh/completion.zsh
+++ linux/zsh/completion.zsh
@@ -4,25 +4,21 @@
 zmodload zsh/complist
 
 autoload -Uz compinit
-zmodload zsh/complist
-_comp_options+=(globdots)        # Include hidden files.
-zle_highlight=('paste:none')
-for dump in "${ZDOTDIR:-$HOME}/.zcompdump"(N.mh+24); do
-  compinit
-done
-compinit -C
+compinit -d "${ZDOTDIR:-$HOME}/.zcompdump"
 
 # setopt GLOB_COMPLETE      # Show autocompletion menu with globs
-setopt GLOB_DOTS
-setopt MENU_COMPLETE        # Automatically highlight first element of completion menu
-setopt AUTO_LIST            # Automatically list choices on ambiguous completion.
-setopt COMPLETE_IN_WORD     # Complete from both ends of a word.
+setopt GLOB_DOTS            # Include hidden files in globbing
+setopt MENU_COMPLETE        # Automatically highlight first element in menu
+setopt AUTO_LIST            # Automatically list choices on ambiguous completion
+setopt COMPLETE_IN_WORD     # Allow completion from both ends of a word
 
 # Ztyle pattern
 # :completion:<function>:<completer>:<command>:<argument>:<tag>
 
 # Define completers
 zstyle ':completion:*' completer _extensions _complete _approximate
+
+zstyle ':completion:*' keep-prefix true
 
 # Use cache for commands using cache
 zstyle ':completion:*' use-cache on
@@ -31,7 +27,6 @@ zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
 zstyle ':completion:*' complete true
 
 zle -C alias-expension complete-word _generic
-bindkey '^Xa' alias-expension
 zstyle ':completion:alias-expension:*' completer _expand_alias
 
 # Allow you to select in a menu
@@ -41,28 +36,31 @@ zstyle ':completion:*' menu select
 zstyle ':completion:*' complete-options true
 zstyle ':completion:*' file-sort modification
 
-zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
-zstyle ':completion:*:*:*:*:messages' format ' %F{purple} -- %d --%f'
-zstyle ':completion:*:*:*:*:warnings' format ' %F{red}-- no matches found --%f'
-
 # Automatically find new executables in path
 zstyle ':completion:*' rehash true
 
+zstyle ':completion:*:*:*:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{blue}-- %D %d --%f'
+zstyle ':completion:*:*:*:*:messages' format '%F{purple}-- %d --%f'
+zstyle ':completion:*:*:*:*:warnings' format '%F{red}-- no matches found --%f'
+
 # Colors for files and directory
-zstyle ':completion:*:*:*:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 
 # Only display some tags for the command cd
 zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
 
 # Required for completion to be in good groups (named after the tags)
 zstyle ':completion:*' group-name ''
-
 zstyle ':completion:*:*:-command-:*:*' group-order aliases builtins functions commands
 
 # See ZSHCOMPWID "completion matching control"
-zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' matcher-list \
+  '' \
+  'm:{a-zA-Z}={A-Za-z}' \
+  'r:|[._-]=* r:|=*' \
+  'l:|=* r:|=*'
 
-zstyle ':completion:*' keep-prefix true
-
-zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
+# Dynamically generate SSH host completions
+zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts \
+  'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
--- linux/zsh/export.zsh
+++ linux/zsh/export.zsh
@@ -3,15 +3,15 @@ HISTSIZE=50000
 SAVEHIST=10000
 
 ## History command configuration
-setopt EXTENDED_HISTORY         # Write the history file in the ":start:elapsed;command" format.
 setopt BANG_HIST                # Treat the '!' character specially during expansion.
-setopt HIST_EXPIRE_DUPS_FIRST   # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt HIST_IGNORE_DUPS         # Don't record an entry that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS     # Never add duplicate entries.
-setopt HIST_FIND_NO_DUPS        # Do not display a line previously found.
-setopt HIST_IGNORE_SPACE        # Ignore commands that start with space
-setopt HIST_SAVE_NO_DUPS        # Don't write duplicate entries in the history file.
-setopt HIST_VERIFY              # Show command with history expansion to user before running it
+setopt EXTENDED_HISTORY         # Write the history file in the ":start:elapsed;command" format.
+setopt APPEND_HISTORY           # Append to the HISTFILE rather than replace it.
 setopt SHARE_HISTORY            # Share history between all sessions.
+setopt HIST_VERIFY              # Show command with history expansion to user before running it
+setopt HIST_EXPIRE_DUPS_FIRST   # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt HIST_IGNORE_SPACE        # Ignore commands that start with space
+setopt HIST_IGNORE_ALL_DUPS     # Never add duplicate entries.
+setopt HIST_SAVE_NO_DUPS        # Don't write duplicate entries in the history file.
+setopt HIST_IGNORE_DUPS         # Don't record an entry that was just recorded again.
+setopt HIST_FIND_NO_DUPS        # Do not display a line previously found.
 setopt HIST_REDUCE_BLANKS       # Remove unnecessary blank lines.
-setopt APPEND_HISTORY
