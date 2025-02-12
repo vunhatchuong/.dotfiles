a6b1658a0d7f1e5af91c820852a8a7e5d4bbd29e
Vu Nhat Chuong <ronnyvu321@gmail.com>
2025-02-12 21:02:58 +0700

    nvim(autopairs): update regex

--- nvim/lua/plugins/autopairs.lua
+++ nvim/lua/plugins/autopairs.lua
@@ -7,7 +7,7 @@ return {
                 disable_when_touch = true,
                 pair_spaces = true,
                 -- matches a single char in the list: %w([{'" (case sensitive)
-                -- touch_regex = "[%w(%[{'\"]",
+                touch_regex = "[%w(%[{'\"]",
             },
             keys = {
                 ["`"] = { escape = false, close = true, pair = "``" },
